79ef4df99b134008730714fe5f71e7fbaa32a523
Andy Lavender <c.andrew.lavender@gmail.com>
2017-09-10 12:25:51 -0400

    Include methods for setting average intersection values for experiments

--- network/tasks.py
+++ network/tasks.py
@@ -967,3 +967,42 @@ def _set_selected_transcript_for_gene(gene_pk):
 
     gene.selected_transcript = transcript
     gene.save()
+
+
+@task
+def set_experiment_intersection_values():
+    '''
+    For each experiment, set average intersection values.
+    '''
+    job = group(_set_experiment_intersection_values.s(exp.pk)
+                for exp in models.Experiment.objects.all())
+    job.apply_async()
+
+
+@task
+def _set_experiment_intersection_values(experiment_pk):
+    '''
+    For a given experiment, set the average intersection values.
+    '''
+    exp = models.Experiment.objects.get(pk=experiment_pk)
+
+    # Remove existing
+    models.ExperimentIntersection.objects.filter(experiment=exp).delete()
+
+    # Create new
+    exp_intersections = []
+    for assembly in \
+            models.Assembly.objects.filter(dataset__experiment=exp).distinct():
+        loci = models.Locus.objects.filter(group__assembly=assembly)
+        for locus in loci:
+            intersection_values = models.DatasetIntersection.objects.filter(
+                locus=locus, dataset__experiment=exp,
+            ).values_list('normalized_value', flat=True)
+            exp_intersections.append(
+                models.ExperimentIntersection(
+                    locus=locus,
+                    experiment=exp,
+                    average_value=numpy.mean(list(intersection_values)),
+                )
+            )
+    models.ExperimentIntersection.objects.bulk_create(exp_intersections)
