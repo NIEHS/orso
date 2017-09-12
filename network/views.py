656d5836ac03a15be3611121adc730aabcdf7475
Andy Lavender <c.andrew.lavender@gmail.com>
2017-09-12 14:48:52 -0400

    Remove unused import

--- network/views.py
+++ network/views.py
@@ -2,7 +2,7 @@ import math
 from collections import defaultdict
 
 import numpy
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic \
     import View, TemplateView, DetailView, CreateView, UpdateView, DeleteView
