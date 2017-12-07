74866ccb2ab8a896c3f49973673f5a2e273d3ef5
Andy Lavender <c.andrew.lavender@gmail.com>
2017-12-07 17:09:05 -0500

    Add command for updating PCA transformed values

---
+++ network/management/commands/add_or_update_pca_transformed_values.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+
+from network.tasks import add_or_update_pca_transformed_values
+
+
+class Command(BaseCommand):
+    help = '''
+        Update PCA tranformed dataset values.
+    '''
+
+    def handle(self, *args, **options):
+        add_or_update_pca_transformed_values()
