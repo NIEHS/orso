648899c9f582deb15b60ad2cad031f9aac5ffb8f
Andy Lavender <c.andrew.lavender@gmail.com>
2017-12-07 17:18:06 -0500

    Add command to get a JSON file describing ENCODE experiments

---
+++ network/management/commands/get_project_from_encode.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from analysis.encode import Encode
+
+
+class Command(BaseCommand):
+    help = '''
+        Call the ENCODE API and save metadata for all datasets to a JSON file.
+    '''
+
+    def add_arguments(self, parser):
+        parser.add_argument('output_json_file', type=str)
+
+    def handle(self, *args, **options):
+        encode = Encode()
+        encode.get_experiments()
+        encode.make_experiment_json(options['output_json_file'])
