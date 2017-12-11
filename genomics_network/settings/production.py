6c4ca189f94dca6974ff965c88cc118e5ec3f034
Andy Lavender <c.andrew.lavender@gmail.com>
2017-12-11 12:10:11 -0500

    Remove paths (inherit from base.py)

--- genomics_network/settings/production.py
+++ genomics_network/settings/production.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PROJECT_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
