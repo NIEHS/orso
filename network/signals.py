f037e1f8d089f5f4a3e8c8061cf0aa7344f623ba
Andy Lavender <c.andrew.lavender@gmail.com>
2017-12-12 10:25:20 -0500

    Disable post-create signal for now

--- network/signals.py
+++ network/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from . import tasks, models
 
 
-@receiver(post_save, sender=models.Dataset)
-def trigger_dataset_processing(sender, instance, created, **kwargs):
-    if created:
-        tasks.process_dataset.delay(instance.id)
+# @receiver(post_save, sender=models.Dataset)
+# def trigger_dataset_processing(sender, instance, created, **kwargs):
+#     if created:
+#         tasks.process_dataset.delay(instance.id)
