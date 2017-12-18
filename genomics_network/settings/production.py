c074ea2574195b69dbad013a216aeda894e00f3f
Andy Lavender <c.andrew.lavender@gmail.com>
2017-12-18 14:30:53 -0500

    Import form base

--- genomics_network/settings/production.py
+++ genomics_network/settings/production.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from genomics_network.settings.base import PROJECT_PATH, PROJECT_ROOT
+
+from genomics_network.settings.base import *  # noqa
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
