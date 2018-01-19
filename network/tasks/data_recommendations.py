9d75e0c56a3a3f5bfb1da67a66495bfd727b18b0
Andy Lavender <c.andrew.lavender@gmail.com>
2018-01-19 14:15:27 -0500

    Use z scores derived from distances to rank recommendations based on primary data

--- network/tasks/data_recommendations.py
+++ network/tasks/data_recommendations.py
@@ -1,3 +1,4 @@
+import numpy
 from celery.decorators import task
 from progress.bar import Bar
 
@@ -10,49 +11,52 @@ def update_dataset_data_scores(datasets, quiet=False):
     '''
     Update or create dataset data distance values.
     '''
-    updated = set()
-
-    bar_max = 0
-    for ds in datasets:
-        bar_max += models.Dataset.objects.filter(
-            assembly=ds.assembly,
-            experiment__experiment_type=ds.experiment.experiment_type,
-        ).count()
-    bar = Bar('Processing', max=bar_max)
+    bar = Bar('Processing', max=len(datasets))
 
     for ds_1 in datasets:
+
+        dataset_to_score = dict()
+
         for ds_2 in models.Dataset.objects.filter(
             assembly=ds_1.assembly,
             experiment__experiment_type=ds_1.experiment.experiment_type,
         ):
 
-            _ds_1, _ds_2 = sorted([ds_1, ds_2], key=lambda x: x.pk)
-            exp_type_1 = _ds_1.experiment.experiment_type
-            exp_type_2 = _ds_2.experiment.experiment_type
+            exp_type_1 = ds_1.experiment.experiment_type
+            exp_type_2 = ds_2.experiment.experiment_type
+
             if all([
-                (_ds_1, _ds_2) not in updated,
-                _ds_1 != _ds_2,
+                ds_1 != ds_2,
                 models.PCATransformedValues.objects.filter(
-                    dataset=_ds_1,
+                    dataset=ds_1,
                     pca__locus_group__group_type=exp_type_1.relevant_regions,
                 ).exists(),
                 models.PCATransformedValues.objects.filter(
-                    dataset=_ds_2,
+                    dataset=ds_2,
                     pca__locus_group__group_type=exp_type_2.relevant_regions,
                 ).exists(),
             ]):
 
-                distance = score.score_datasets_by_pca_distance(_ds_1, _ds_2)
-                models.DatasetDataDistance.objects.update_or_create(
-                    dataset_1=_ds_1,
-                    dataset_2=_ds_2,
-                    defaults={
-                        'distance': distance,
-                    },
-                )
-                updated.add((_ds_1, _ds_2))
+                distance = score.score_datasets_by_pca_distance(ds_1, ds_2)
+                dataset_to_score[ds_2] = distance
 
-            bar.next()
+        distances = list(dataset_to_score.values())
+
+        average = numpy.mean(distances)
+        sd = numpy.std(distances)
+
+        for ds_2, distance in dataset_to_score.items():
+            z_score = (distance - average) / sd
+
+            models.DatasetDataDistance.objects.update_or_create(
+                dataset_1=ds_1,
+                dataset_2=ds_2,
+                defaults={
+                    'distance': z_score,
+                },
+            )
+
+        bar.next()
 
     bar.finish()
 
@@ -62,16 +66,7 @@ def update_experiment_data_scores(experiments):
     '''
     Update or create experiment data distance values.
     '''
-    updated = set()
-
-    bar_max = 0
-    for exp in experiments:
-        assemblies = models.Assembly.objects.filter(dataset__experiment=exp)
-        bar_max += models.Experiment.objects.filter(
-            dataset__assembly__in=assemblies,
-            experiment_type=exp.experiment_type,
-        ).count()
-    bar = Bar('Processing', max=bar_max)
+    bar = Bar('Processing', max=len(experiments))
 
     for exp_1 in experiments:
         assemblies = models.Assembly.objects.filter(dataset__experiment=exp_1)
@@ -80,33 +75,30 @@ def update_experiment_data_scores(experiments):
             experiment_type=exp_1.experiment_type,
         ):
 
-            _exp_1, _exp_2 = sorted([exp_1, exp_2], key=lambda x: x.pk)
-            rr_1 = _exp_1.experiment_type.relevant_regions
-            rr_2 = _exp_2.experiment_type.relevant_regions
+            rr_1 = exp_1.experiment_type.relevant_regions
+            rr_2 = exp_2.experiment_type.relevant_regions
             if all([
-                (_exp_1, _exp_2) not in updated,
-                _exp_1 != _exp_2,
+                exp_1 != exp_2,
                 models.PCATransformedValues.objects.filter(
-                    dataset__experiment=_exp_1,
+                    dataset__experiment=exp_1,
                     pca__locus_group__group_type=rr_1,
                 ).exists(),
                 models.PCATransformedValues.objects.filter(
-                    dataset__experiment=_exp_2,
+                    dataset__experiment=exp_2,
                     pca__locus_group__group_type=rr_2,
                 ).exists(),
             ]):
 
                 distance = score.score_experiments_by_pca_distance(
-                    _exp_1, _exp_2)
+                    exp_1, exp_2)
                 models.ExperimentDataDistance.objects.update_or_create(
-                    experiment_1=_exp_1,
-                    experiment_2=_exp_2,
+                    experiment_1=exp_1,
+                    experiment_2=exp_2,
                     defaults={
                         'distance': distance,
                     },
                 )
-                updated.add((_exp_1, _exp_2))
 
-            bar.next()
+        bar.next()
 
     bar.finish()
--- network/tasks/metadata_recommendations.py
+++ network/tasks/metadata_recommendations.py
@@ -9,15 +9,7 @@ def update_dataset_metadata_scores(datasets):
     '''
     Update or create dataset metadata distance values.
     '''
-    updated = set()
-
-    bar_max = 0
-    for ds in datasets:
-        bar_max += models.Dataset.objects.filter(
-            assembly=ds.assembly,
-            experiment__experiment_type=ds.experiment.experiment_type,
-        ).count()
-    bar = Bar('Processing', max=bar_max)
+    bar = Bar('Processing', max=len(datasets))
 
     onts = []
     onts.append(models.Ontology.objects.get(name='cell_ontology'))
@@ -28,20 +20,19 @@ def update_dataset_metadata_scores(datasets):
     gene_ont = models.Ontology.objects.get(
         name='gene_ontology').get_ontology_object()
 
+    epi_ont = models.Ontology.objects.get(
+        name='epigenetic_modification_ontology').get_ontology_object()
+
     for ds_1 in datasets:
         for ds_2 in models.Dataset.objects.filter(
             assembly=ds_1.assembly,
             experiment__experiment_type=ds_1.experiment.experiment_type,
         ):
 
-            _ds_1, _ds_2 = sorted([ds_1, ds_2], key=lambda x: x.pk)
-            if all([
-                (_ds_1, _ds_2) not in updated,
-                _ds_1 != _ds_2,
-            ]):
+            if ds_1 != ds_2:
 
-                exp_1 = models.Experiment.objects.get(dataset=_ds_1)
-                exp_2 = models.Experiment.objects.get(dataset=_ds_2)
+                exp_1 = models.Experiment.objects.get(dataset=ds_1)
+                exp_2 = models.Experiment.objects.get(dataset=ds_2)
 
                 total_sim = 0
 
@@ -60,15 +51,20 @@ def update_dataset_metadata_scores(datasets):
                 if gene_ont_sim:
                     total_sim += gene_ont_sim
 
+                epi_ont_sim = epi_ont.get_word_similarity(
+                    exp_1.target, exp_2.target, metric='lin')
+                if epi_ont_sim:
+                    total_sim += epi_ont_sim
+
                 models.DatasetMetadataDistance.objects.update_or_create(
-                    dataset_1=_ds_1,
-                    dataset_2=_ds_2,
+                    dataset_1=ds_1,
+                    dataset_2=ds_2,
                     defaults={
                         'distance': total_sim,
                     },
                 )
 
-            bar.next()
+        bar.next()
 
     bar.finish()
 
@@ -78,16 +74,7 @@ def update_experiment_metadata_scores(experiments):
     '''
     Update or create experiment metadata distance values.
     '''
-    updated = set()
-
-    bar_max = 0
-    for exp in experiments:
-        assemblies = models.Assembly.objects.filter(dataset__experiment=exp)
-        bar_max += models.Experiment.objects.filter(
-            dataset__assembly__in=assemblies,
-            experiment_type=exp.experiment_type,
-        ).count()
-    bar = Bar('Processing', max=bar_max)
+    bar = Bar('Processing', max=len(experiments))
 
     onts = []
     onts.append(models.Ontology.objects.get(name='cell_ontology'))
@@ -105,38 +92,33 @@ def update_experiment_metadata_scores(experiments):
             experiment_type=exp_1.experiment_type,
         ):
 
-            _exp_1, _exp_2 = sorted([exp_1, exp_2], key=lambda x: x.pk)
-            if all([
-                (_exp_1, _exp_2) not in updated,
-                _exp_1 != _exp_2,
-            ]):
+            if exp_1 != exp_2:
 
                 total_sim = 0
 
                 cell_ont_sims = []
                 for ont_obj in cell_ont_list:
                     sim = ont_obj.get_word_similarity(
-                        _exp_1.cell_type, _exp_2.cell_type, metric='lin')
+                        exp_1.cell_type, exp_2.cell_type, metric='lin')
                     if sim:
                         cell_ont_sims.append(sim)
                 if cell_ont_sims:
                     total_sim += max(cell_ont_sims)
 
                 gene_ont_sim = gene_ont.get_word_similarity(
-                    _exp_1.target, _exp_2.target, metric='jaccard',
+                    exp_1.target, exp_2.target, metric='jaccard',
                     weighting='information_content')
                 if gene_ont_sim:
                     total_sim += gene_ont_sim
 
                 models.ExperimentMetadataDistance.objects.update_or_create(
-                    experiment_1=_exp_1,
-                    experiment_2=_exp_2,
+                    experiment_1=exp_1,
+                    experiment_2=exp_2,
                     defaults={
                         'distance': total_sim,
                     },
                 )
-                updated.add((_exp_1, _exp_2))
 
-            bar.next()
+        bar.next()
 
     bar.finish()
