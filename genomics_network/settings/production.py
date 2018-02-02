1896777b6b799441c91b95b7c928dcebf2a0b84b
Andy Lavender <c.andrew.lavender@gmail.com>
2018-02-02 13:43:58 -0500

    Use appropriate static file path

--- genomics_network/settings/production.py
+++ genomics_network/settings/production.py
@@ -106,18 +106,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
-)
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
