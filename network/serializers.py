75a1c0e5590928a704d1669ead4534237e4f61c1
Andy Lavender <c.andrew.lavender@gmail.com>
2018-02-14 16:03:16 -0500

    Read PCA information from pre-compiled JSON

--- network/serializers.py
+++ network/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import serializers
 
 from . import models
@@ -26,51 +28,13 @@ class PCAPlotSerializer(serializers.ModelSerializer):
     components = serializers.SerializerMethodField('_components')
 
     def _pca_plot(self, pca):
-
-        plot = []
-
-        datasets = models.Dataset.objects.filter(pcatransformedvalues__pca=pca)
-        for ds in datasets:
-            plot.append({
-                'experiment_name': ds.experiment.name,
-                'dataset_name': ds.name,
-                'experiment_pk': ds.experiment.pk,
-                'dataset_pk': ds.pk,
-                'experiment_cell_type': ds.experiment.cell_type,
-                'experiment_target': ds.experiment.target,
-                'transformed_values':
-                    ds.pcatransformedvalues_set
-                      .get(pca=pca).transformed_values,
-            })
-
-        return plot
+        return json.loads(pca.plot)['plot']
 
     def _explained_variance(self, pca):
-        return pca.pca.explained_variance_ratio_
+        return json.loads(pca.plot)['explained_variance']
 
     def _components(self, pca):
-        components = []
-
-        if pca.locus_group.group_type in ['genebody', 'promoter', 'mRNA']:
-            locus_names = [
-                x.locus.transcript.gene.name for x in
-                models.PCALocusOrder.objects.filter(pca=pca).order_by('order')
-            ]
-        elif pca.locus_group.group_type in ['enhancer']:
-            locus_names = [
-                x.locus.enhancer.name for x in
-                models.PCALocusOrder.objects.filter(pca=pca).order_by('order')
-            ]
-
-        for _component in pca.pca.components_:
-            components.append(
-                sorted(
-                    zip(locus_names, _component),
-                    key=lambda x: -abs(x[1]),
-                )[:20]
-            )
-
-        return components
+        return json.loads(pca.plot)['components']
 
     class Meta:
         model = models.PCA
