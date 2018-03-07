75c66aee869f54da777e0a2c6ee0771375ea79de
Andy Lavender <c.andrew.lavender@gmail.com>
2018-03-07 16:58:26 -0500

    Modify to use new update method

--- network/management/commands/add_or_update_pca.py
+++ network/management/commands/add_or_update_pca.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
-from network import models
-from network.tasks.update_pca import add_or_update_pca
+from network.tasks.update_pca import add_or_update_pcas
 
 
 class Command(BaseCommand):
@@ -12,20 +10,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--project_names',
-            action='store',
-            dest='project_names',
-            type=str,
-            help='Comma-delimited list of project names to consider',
-        )
-        parser.add_argument(
-            '--project_ids',
-            action='store',
-            dest='project_ids',
-            type=str,
-            help='Comma-delimited list of project IDs to consider',
-        )
-        parser.add_argument(
             '--threads',
             action='store',
             dest='threads',
@@ -34,18 +18,4 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # Get all datasets with project names and IDs
-        query = Q()
-        if options['project_names'] or options['project_ids']:
-            if options['project_names']:
-                for name in options['project_names'].split(','):
-                    query |= Q(experiment__project__name=name)
-            if options['project_ids']:
-                for pk in options['project_ids'].split(','):
-                    query |= Q(experiment__project__pk=pk)
-
-        # Get all datasets without revoked
-        query = (query) & Q(revoked=False)
-
-        datasets = models.Dataset.objects.filter(query)
-        add_or_update_pca(datasets, threads=options['threads'])
+        add_or_update_pcas(threads=options['threads'])
