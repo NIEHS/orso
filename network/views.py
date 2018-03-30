c0dc30ee68b32328d48b274497875732471a2f86
Andy Lavender <c.andrew.lavender@gmail.com>
2018-03-30 11:58:47 -0400

    Comparison with pagination requires set ordering

--- network/views.py
+++ network/views.py
@@ -601,7 +601,7 @@ class ExperimentList(AddMyUserMixin, ListView):
         if self.form.is_valid():
             query &= self.form.get_query()
 
-        qs = self.model.objects.filter(query).distinct()
+        qs = self.model.objects.filter(query).distinct().order_by('pk')
 
         paginator = Paginator(qs, self.get_paginate_by(qs))
         page = self.request.GET.get('page')
