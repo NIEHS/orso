f51d77cb35e315a86842fb31c49f838175012326
Andy Lavender <c.andrew.lavender@gmail.com>
2018-05-11 17:10:43 -0400

    Remove unused urls

--- network/urls.py
+++ network/urls.py
@@ -49,8 +49,6 @@ urlpatterns = [
         name='explore_network'),
     url(r'^explore/overview/$', views.ExploreOverview.as_view(),
         name='explore_overview'),
-    url(r'^explore/recommendations/$', views.ExploreRecommendations.as_view(),
-        name='explore_recommendations'),
 
     url(r'^experiment/(?P<pk>\d+)/similar_experiments/$',
         views.SimilarExperiments.as_view(), name='similar_experiments'),
